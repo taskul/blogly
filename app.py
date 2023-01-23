82600a223da7cd05b9daecdab27f21fe9257b89b
tastasterone <tastankulmeshkenov@gmail.com>
2023-01-22 21:53:01 -0600

    update blog post routes, create Post model, update css

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 """Blogly application."""
 from flask import Flask, render_template, redirect, request
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, Users
+from models import db, connect_db, Users,Post
+from datetime import datetime, date
 
 
 app = Flask(__name__)
@@ -53,12 +54,13 @@ def create_new_user():
         return redirect(f"/users")
     return redirect("/users/new")
 
-
+# --------------------------------------User profile page--------------------------------------------
 @app.route("/users/<int:user_id>")
 def show_user_page(user_id):
     """Show user profile page"""
     user = Users.query.get_or_404(user_id)
-    return render_template("users/user_page.html", user=user)
+    posts = Post.query.filter(Post.user_id == user.id)
+    return render_template("users/user_page.html", user=user, posts=posts)
 
 
 @app.route("/users/<int:user_id>/edit")
@@ -84,11 +86,48 @@ def update_user(user_id):
 
 @app.route("/users/<int:user_id>/delete")
 def delete_user(user_id):
+    '''Delete current user'''
     user = Users.query.get_or_404(user_id)
     db.session.delete(user)
     db.session.commit()
     return redirect("/users")
 
+# --------------------------------------User posts--------------------------------------------
+
+@app.route('/users/<int:user_id>/posts/new')
+def new_post_form(user_id):
+    user = Users.query.get(user_id)
+    posts = Post.query.filter(Post.id == user_id)
+    return render_template('posts/new_post.html', user=user, posts=posts)
+
+@app.route('/users/<int:user_id>/posts/new', methods=['POST'])
+def create_new_post(user_id):
+    if request.form['title'] and request.form['content']:
+        title = request.form['title']
+        content = request.form['content']
+        current_date = date.today()
+        time_and_date = datetime.now()
+        time = time_and_date.strftime("%H:%M:%S")
+        new_post = Post(title=title, 
+                        content=content, 
+                        create_at=f'{current_date} - {time}',
+                        user_id=user_id)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(f'/users/{user_id}')
+    return redirect(f'/users/{user_id}/posts/new')
+
+@app.route('/posts/<int:post_id>')
+def show_post(post_id):
+    post = Post.query.get(post_id)
+    user = Users.query.get(post.id)
+    return render_template('posts/view_post.html', post=post, user=user)
+
+@app.route('/posts/<int:post_id>/edit')
+def edit_post(post_id):
+    post = Post.query.get(post_id)
+    user = Users.query.get(post.id)
+    return render_template('posts/edit_post.html', post=post, user=user)
 
 if __name__ == "__main__":
     app.run(debug=True)
